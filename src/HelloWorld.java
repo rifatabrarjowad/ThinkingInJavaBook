8836ae07ca37ea2aa12c5768256902feadcd792d
dev <rajdiprifat2004@gmail.com>
2021-05-12 23:09:32 +0600

    adding file

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("1. java");
+        System.out.print("2. java \t" );
+        System.out.println("3. java");
+        System.out.println("4. java");
+    }
+}
